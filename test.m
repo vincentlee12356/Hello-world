e584da9dd3585760f7cd48bcafefeee089a4bc8c
vincentlee12356 <70370844+vincentlee12356@users.noreply.github.com>
2020-09-10 15:24:52 +0800

    Add files via upload

---
+++ test.m
@@ -0,0 +1,8 @@
+kplot=[];yplot=[];
+for k=0.1:0.05:1
+    beta=k;
+    y=24*(1-1.335*beta+1.947*beta^2-1.701*beta^3+0.956*beta^4-0.254*beta^5);
+    kplot=[kplot,k];
+    yplot=[yplot,y];
+end
+plot(kplot,yplot)
